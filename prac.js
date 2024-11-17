a04faabb156f8fbd8e804fc4b1ef2bfa59c76d94
Madhukarkumar12 <madhukarkumar26072001@gmail.com>
2024-11-17 18:54:36 +0530

    icon

--- prac.js
+++ prac.js
@@ -85,19 +85,7 @@ function shops(){
     
 }
 
-// function blogs(){
-//     mainPage.style.display="none";
-//     card.style.display="none";
-//     card2.style.display="none";
-//     blog.style.display="block";
-//     about.style.display="none"
 
-//     document.getElementById("blog").style.color="rgb(2, 173, 173)";
-//     document.getElementById("home").style.color="black";
-//     document.getElementById("shop").style.color="black";
-//     document.getElementById("contact").style.color="black";
-//     document.getElementById("about").style.color="black"
-// }
 
 function abouts(){
     mainPage.style.display="none";
@@ -170,44 +158,9 @@ function addCart(){
     location.reload()
 }
 
-// let moreInfoButton = document.querySelector('.more-info');
-// let popup = document.querySelector('carto');
-// let closeButton = document.querySelector('.popup-close');
 
-// moreInfoButton.addEventListener('click', () => {
-//     popup.style.display = 'block';
-//     popup.style.position='fixed';
-//     popup.style.top= '50%';
-//     popup.style.left= '50%';
-//     popup.style.transform='translate(-50%, -50%)';
-//     popup.style.backgroundColor= 'white';
-//     popup.style.padding='20px';
-//     popup.style.borderRadius='5px';
-//     popup.style.boxShadow= '0px 0px 10px rgba(0, 0, 0, 0.5)';
-    
-//     setTimeout(() => {
-//         popup.style.display = 'none';
-//     }, 5000);
-// });
 function moreInfo(button){
-    // mainPage.style.display="none";
-    // card.style.display="none";
-    // card2.style.display="none";
-    // blog.style.display="none";
-    // about.style.display="none";
-    // contact.style.display="none";
-    // document.querySelector(".cart").style.display="flex";
-    // document.querySelector(".cart").style.position="fixed";
-    // document.querySelector(".cart").style.top="50%";
-    // document.querySelector(".cart").style.left="50%";
-    // document.querySelector(".cart").style.transform="translate(-50%, -50%)";
-    // document.querySelector(".cart").style.backgroundColor="white";
-    // document.querySelector(".cart").style.padding="20px";
-    // document.querySelector(".cart").style.borderRadius="5px";
-    // document.querySelector(".cart").style.boxShadow="0px 0px 10px rgba(0, 0, 0, 0.5)";
-    // document.getElementById("more-sections").style.display="none"
-    // document.querySelector(".pop").style.display="flex"
-    // document.getElementById("show-more").style.display="none"
+    
     
     const imageUrl=button.getAttribute("data-image");
     const popImage=document.getElementById("new-Img");
@@ -237,16 +190,7 @@ function moreInfo(button){
     
 
     setTimeout(() => {
-        // document.querySelector(".cart").style.display="none";
-        // document.querySelector(".pop").style.display="none"
-        // document.getElementById("show-more").style.display="block"
-        // mainPage.style.display="flex";
-        // card.style.display="block";
-        // card2.style.display="block";
-        // document.getElementById("more-sections").style.display="block"
-        // blog.style.display="block";
-        // about.style.display="block";
-        // contact.style.display="block";
+       
         document.getElementById("popupOverlay").style.display = "none";
         document.getElementById("popupContent").style.display = "none";
         
@@ -265,190 +209,13 @@ function closeButton(){
 }
 
 document.getElementById("popup_Close").onclick = function() {
-    // document.getElementById("popupOverlay").style.display = "none";
-    // document.getElementById("popupContent").style.display = "none";
-    // document.querySelector(".cart").style.display="none";
-    // mainPage.style.display="flex";
-    // card.style.display="block";
-    // card2.style.display="block";
-    // document.getElementById('show-more').style.display='block';
-    // window.location.href = "prac.html";
+   
     document.getElementById('carto').style.display='none'
     homes();
 
 };
 
-// closeButton.addEventListener('click', () => {
-//     popup.style.display = 'none';
-// });
 
-
-
-// let sectionIndex = 0;
-// const sections = [
-//     `
-//     <div class="trend">
-//             <div class="head">
-//                 <h1>Trends <span>Foot Wear</span></h1>
-//             </div>
-//             <div class="card">
-//                 <div class="crd">
-//                     <img src="slipper.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Slipper</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-half-stroke"></i> <br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()" >more</button>
-//                     </div>
-//                 </div>
-
-                
-
-
-//                  <div class="crd">
-//                     <img src="shoes.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Shoes</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-half-stroke"></i> <br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()">more</button>
-//                     </div>
-//                  </div>
-
-//                  <div class="crd">
-//                     <img src="crockse.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Crocks</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-sharp-half-stroke"></i><br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()">more</button>
-//                     </div>
-//                  </div>
-
-//             </div>
-//          </div>
-//     `,
-//     `
-//     <div class="trend">
-//             <div class="head">
-//                 <h1>Trends <span>Bags</span></h1>
-//             </div>
-//             <div class="card">
-//                 <div class="crd">
-//                     <img src="bagpack.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Backpacks</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-half-stroke"></i> <br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()" >more</button>
-//                     </div>
-//                 </div>
-
-                
-
-
-//                  <div class="crd">
-//                     <img src="handbag.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Handbags</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-half-stroke"></i> <br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()">more</button>
-//                     </div>
-//                  </div>
-
-//                  <div class="crd">
-//                     <img src="travelling_bag.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Travel-bags</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-sharp-half-stroke"></i><br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()">more</button>
-//                     </div>
-//                  </div>
-
-//             </div>
-//          </div>
-//     `,
-//     `
-//        <div class="trend">
-//             <div class="head">
-//                 <h1>Trends <span>Accessories</span></h1>
-//             </div>
-//             <div class="card">
-//                 <div class="crd">
-//                     <img src="watch.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Watches</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-half-stroke"></i> <br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()" >more</button>
-//                     </div>
-//                 </div>
-
-                
-
-
-//                  <div class="crd">
-//                     <img src="sunglass.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Sun-glass</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-half-stroke"></i> <br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()">more</button>
-//                     </div>
-//                  </div>
-
-//                  <div class="crd">
-//                     <img src="belt.webp" alt="" onclick="show(this)">
-//                     <div class="crdText">
-//                         <h2>Trending Belt</h2>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star"></i>
-//                         <i class="fa-solid fa-star-sharp-half-stroke"></i><br>
-//                         <button onclick="addCart()">Add to Cart</button>
-//                         <button class="more-Info" onclick="moreInfo()">more</button>
-//                     </div>
-//                  </div>
-
-//             </div>
-//          </div>
-//     `
-// ];
 
 // Assuming there's a main container in the HTML with id "main-container"
 const mainContainer = document.getElementById("more-sections");
@@ -592,17 +359,7 @@ function showMore(){
 
 
 
-// function showMore() {
-//     if (sectionIndex < sections.length) {
-//         document.getElementById("more-sections").innerHTML += sections[sectionIndex];
-//         sectionIndex++;
-//     }
 
-//     if (sectionIndex >= sections.length) {
-//         document.getElementById("show-more").disabled = true;
-//         document.getElementById("go-home").style.display = "block";
-//     }
-// }
 
 function goHome() {
     
