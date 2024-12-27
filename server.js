1bede94a1519e60507d092bf1626cef2bdb72c03
Madhukarkumar12 <madhukarkumar26072001@gmail.com>
2024-12-27 13:41:15 +0530

    icon

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
+// allows all origin
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
